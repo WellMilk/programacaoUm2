e77fc6a03fa2cb997d080da1e6e64ea552ea3ad7
WellMilk <wellconta3773@gmail.com>
2026-08-19 13:35:09 -0300

    Resolução dos exercicios com if

---
+++ src/exercicios_condicional_if/Ex02.java
@@ -0,0 +1,10 @@
+package exercicios_condicional_if;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// TODO Stub de método gerado automaticamente
+
+	}
+
+}
---
+++ src/exercicios_condicional_if/Exercicio1.java
@@ -0,0 +1,9 @@
+package exercicios_condicional_if;
+
+public class Exercicio1 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
